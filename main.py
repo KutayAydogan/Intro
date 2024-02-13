61bca7471c99d8844497a86587f78a1101587afc
KutayAydogan <kutayaydogan@gmail.com>
2024-02-13 13:08:45 +0300

    /Fibonacci

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-names=["Bora","Hasan","Kutay","Rand"]
-print(names)
-new_names= [x if x!= "Rand" else "Ramazan" for x in names]
-print(new_names)
-if len(new_names) < 5:
-    new_names.append("Kemal")
-    print(new_names)
-new_names.sort()
-print(new_names)
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonacci(n-1)+fibonacci(n-2)
+for n in range (0,10):
+    print(n,"=",fibonacci(n))
