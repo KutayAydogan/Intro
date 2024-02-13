a8ae449bafdf1c94e32af5cce5a6a7e16f06b4c5
KutayAydogan <kutayaydogan@gmail.com>
2024-02-13 15:56:52 +0300

    /Prime_number_checker

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-def fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    elif n > 2:
-        return fibonacci(n-1)+fibonacci(n-2)
-for n in range (0,10):
-    print(n,"=",fibonacci(n))
+number = int(input("Enter the number you want to check"))
+if number <= 0:
+    print("Select a postive and greater than 0 number")
+if number == 1:
+    print("Number is not prime")
+elif number > 1:
+    for x in range(2,number):
+        if (number%x) == 0:
+            print("Number is not prime")
+            break
+    else: 
+         print("Number is prime")
