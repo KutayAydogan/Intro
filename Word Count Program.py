94637751d9dc25b556f7256e27a9abaf4e47792b
KutayAydogan <kutayaydogan@gmail.com>
2024-02-17 00:12:32 +0300

    /Word_Counting_Program

--- Word Count Program.py
+++ Word Count Program.py
@@ -1,8 +1,11 @@
-import os
-def current():
-    print("Current working")
-    print(os.getcwd())
-    print()
-current()
-os.chdir('../')
-current
+#import os
+words = ("Mom", "Dad", "Brother", "Sister", "Cousin", "Cousin", "Cousin", "Sister", "Brother")
+word_count = {}
+for word in words:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+for word, count in word_count.items():
+    print(f"{word}: {count}")
+#C:\Users\kutay\Desktop\Intro\Story for Word Counting.md
