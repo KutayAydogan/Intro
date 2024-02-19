20b0166cde5c69b2b3281486723a4ee3f3cc84b8
KutayAydogan <kutayaydogan@gmail.com>
2024-02-19 10:45:03 +0300

    /Word

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
-import random
-choices = ["rock","paper","scissors"]
-while True:
-    pc_choice = random.choice(choices)
-    your_choice = input("Please select rock, paper or scissors:")
-    your_choice = your_choice.lower()
-    print(f"\nYou chose {your_choice}, computer chose {pc_choice}.\n")
-    if pc_choice == your_choice:
-         print("It's a tie")
-    elif your_choice == "rock":
-        if pc_choice == "paper":
-             print("Rock loses to Paper, You lose.")
-        else:
-            print("Rock beats Scissors, You win.")
-    elif your_choice == "paper":
-        if pc_choice == "rock":
-             print("Paper beats Rock, You win.")
-        else:
-            print("Paper loses to Scissors, You lose.")
-    elif your_choice == "scissors":
-        if pc_choice == "rock":
-            print("Scissors loses to Rock, You lose.")
-        else:
-            print("Scissors beats Paper, You win.")
-    play_again = input("Play again? (y for yes , n for no): ")
-    if play_again.lower() != "y":
-        break
+import os
+filename = r"C:\Users\kutay\Desktop\Intro\Story for Word Counting.md"
+if not os.path.isfile(filename):
+    print(f"Error: File '{filename}' not found.")
+else:
+    word_counts = {}
+    with open(filename, 'r') as file:
+        text = file.read()
+        text = text.replace(",","")
+        text = text.replace(".","")
+        text = text.lower()
+        words = text.split()
+        for word in words:
+            if word in word_counts:
+                word_counts[word] += 1
+            else:
+                word_counts[word] = 1
+    for word, count in word_counts.items():
+        print(f"{word}: {count}")
